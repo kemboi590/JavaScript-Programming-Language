489955bfcac1d57eeb172cf6d800f66680ec9f29
kemboi590 <bkemboi590@gmail.com>
2023-03-14 17:55:06 +0300

    Variables in Js

---
+++ Variables/App.js
@@ -0,0 +1,48 @@
+/* 
+VARIABLES IN JAVASCRIPT
+Let 
+var
+const
+
+*/
+
+//Let keyword
+let myName = "Kemboi"
+console.log(myName)
+
+//reassigning a variable declared using Let key word
+let hisName = "David"
+ //David
+console.log(hisName)
+hisName = myName
+ //kemboi
+console.log(hisName)
+
+
+//var key word
+
+var student = "Kimani"
+//kimani
+console.log(student) 
+var student2 = "Kibet"
+//kibet 
+console.log(student2) 
+//reasigning variable
+student2 = student 
+ //kimani
+console.log(student2)
+
+//Const key word
+
+const num = 123
+console.log(num)
+const nam = 456
+console.log(nam)
+//const cannot be reassigned
+// num = nam
+// console.log(num)
+
+
+
+
+
